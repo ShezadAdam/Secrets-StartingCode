702f468e468795bf832490f7c3903476035ef352
Shezad Ahmed <shezadadam@gmail.com>
2020-12-01 11:43:27 +0000

    passport.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 //jshint esversion:6
 const path = require('path')
-require('dotenv').config({ path1: __dirname + "\.env" })
+require('dotenv').config({
+  path1: __dirname + "\.env"
+})
 console.log(require('dotenv').config);
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -16,29 +20,46 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-// console.log("SECRET " + process.env.SECRET);
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 });
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "email is manadatory"]
+  username: {
+    type: String
+    // required: [true, "email is manadatory"]
   },
   password: {
-    type: String,
-    required: [true, "content is required"]
+    type: String
+    // required: [true, "content is required"]
   }
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 // const secret = process.env.SECRET;
 //
 // userSchema.plugin(encrypt, { secret: secret , encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res) {
@@ -53,64 +74,85 @@ app.get("/register", function(req, res) {
   res.render("register");
 }); //app.get("/register", function(req,res) {
 
-app.post("/register", function(req,res) {
-  //console.log(req.body.username);
-  const username = req.body.username;
-  const password = req.body.password;
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 
 
-  bcrypt.hash(password, 10, function(err, hash) {
-    // Store hash in database
-    const userToInsert = new User({
-      email: req.body.username,
-      password: hash
-    });
-    userToInsert.save(function(err) {
-          if (err) {
-            res.send(err);
-              } else {
-          res.render("secrets");
-          }
-      });
+app.post("/register", function(req, res) {
+
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, registeredUser) {
+
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      })
+    }
   });
-
-
-
-
 
 }); //app.post("/register", function(req,res) {
 
-  app.post("/login", function(req,res){
+app.post("/login", function(req, res) {
 
-    const username = req.body.username;
-    var password = req.body.password;
-
-
-
-    User.findOne({email: username }, function(err, foundUser) {
-      if (err) {
-    res.send(err);
+  if (!req.body.username) {
+    res.json({
+      success: false,
+      message: "Username was not given"
+    })
+  } else {
+    if (!req.body.password) {
+      res.json({
+        success: false,
+        message: "Password was not given"
+      })
     } else {
-      if (foundUser){
 
-        bcrypt.compare(password, foundUser.password, function(err, foundpassword) {
-          // console.log(password + " " + foundUser.password );
-    if(foundpassword) {
-     // Passwords match
-      res.render("secrets");
-    } else {
-     // Passwords don't match
-     res.send("Invalid password!");
+      const newLoginUser = new User({
+        usrname: req.body.username,
+        password: req.body.password
+      });
+
+      passport.serializeUser(function(newLoginUser, done) {
+        done(null, newLoginUser);
+      });
+
+      passport.deserializeUser(function(newLoginUser, done) {
+        done(null, newLoginUser);
+      });
+
+      req.login(newLoginUser, function(err) {
+
+        if (err) {
+          console.log(err);
+          res.json({
+            success: false,
+            message: err
+          });
+        } else {
+          passport.authenticate("local")(req, res, function() {
+            res.redirect("/secrets");
+          })
+        }
+
+      }); //req.login(newLoginUser, function(err){
     }
+  } //if(!req.body.username){
+}); //app.post("/login", function(req,res){
+
+
+  app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect("/");
   });
-      }
-    }
-  });//User.findOne({email: username }, function(err, foundUser) {
-
-  });//app.post("/login", function(req,res){
-
-
-
 
 
 
